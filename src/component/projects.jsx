86da757d86b738126ce7712c54707d3d5922c569
diego_maquill <diego.maquill@gmail.com>
2019-11-06 17:39:24 -0500

    full-stack

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -51,7 +51,7 @@ export default class Projects extends React.Component {
 								<h5>This is JavaScript, HTML, CSS first generation project in which uses different APIs to search information for users wishing to move to the USA for the first time. Myself being a immigrant was difficult to assimilate to the American culture at first. We immigrants tend to look for things common to us from back home. With the right marketing, nuBie can we a go to web application for people looking for ethnic cultural matching within the USA</h5>
 							</div>
 							<div class="col-12 col-sm-12 col-md-4 ">
-								<h5>This project is a combination of NodeJS, MongoDB, Express, Passport, HTML ,and Bootstrap CSS. An eCommerce application for farmers market. The idea was to create a shopping cart for people who want to shop at farmer's market major cities throughout the United States. Most of these farmers market are rotating farmers market and they do not have access to sell through website application. </h5>
+								<h5>Final Full Stack project. This is a combination of NodeJS, MongoDB, Express, Passport, HTML ,and Bootstrap CSS. An eCommerce application for farmers market. The idea was to create a shopping cart for people who want to shop at farmer's market major cities throughout the United States. Most of these farmers market are rotating farmers market and they do not have access to sell through website application. </h5>
 							</div>
 							<div class="col-12 col-sm-12 col-md-4 ">
 								<h5>This a compilation of short programming execises done for my coding bootcamp. You will find anything from basic HTML & CSS to complex execises using NodeJS, Firebase, MySQL, MongoDB and much more.  </h5>
